8b151fcd0af4e6afdcf3389e7da9c89c9c33712a
EdmundLuii <edmund.lui1998@gmail.com>
2019-10-06 13:10:29 -0400

    final version of app

--- app/src/main/java/com/example/quicknready/camera_messengerActivity.java
+++ app/src/main/java/com/example/quicknready/camera_messengerActivity.java
@@ -49,10 +49,6 @@ public class camera_messengerActivity extends AppCompatActivity {
 
     }
 
-    public void open_chat(View view){
-        Intent intent = new Intent(this, chatActivity.class);
-        startActivity(intent);
-    }
     public void open_chat(View view) {
         Intent intent = new Intent(this, chatActivity.class);
         startActivity(intent);

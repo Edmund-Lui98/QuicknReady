f13d026180db761cbddfece36e066a632d541a51
EdmundLuii <edmund.lui1998@gmail.com>
2019-10-05 23:22:58 -0400

    finished firebase authentication for login

--- app/src/main/java/com/example/quicknready/LoginActivity.java
+++ app/src/main/java/com/example/quicknready/LoginActivity.java
@@ -1,14 +1,80 @@
 package com.example.quicknready;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class LoginActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    EditText user, pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        user = findViewById(R.id.login);
+        pass = findViewById(R.id.password);
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    public void onClickLogin(View view) {
+        if (user.length() > 0 && pass.length() > 0) {
+            mAuth.signInWithEmailAndPassword(user.getText().toString(), pass.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        logIn();
+                    } else {
+                        Toast toast = Toast.makeText(getApplicationContext(), "Invalid Email or Password. Please try again.", Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                }
+            });
+        } else {
+            Toast toast_2 = Toast.makeText(getApplicationContext(), "Please Fill up the username and password fields before.", Toast.LENGTH_SHORT);
+            toast_2.show();
+        }
+
+    }
+
+    public void logIn() {
+        Intent myIntent = new Intent(this, MessengerActivity.class);
+        this.startActivity(myIntent);
+    }
+
+    public void onClickRegister(View view) {
+        if (user.length() > 0 && pass.length() > 0) {
+            mAuth.createUserWithEmailAndPassword(user.getText().toString(), pass.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        FirebaseDatabase.getInstance().getReference().child("users").child(task.getResult().getUser().getUid()).setValue("Empty");
+                        logIn();
+                    } else {
+                        Toast toast = Toast.makeText(getApplicationContext(), "Invalid Email or Password. Please try again.", Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                }
+            });
+        } else {
+            Toast toast_2 = Toast.makeText(getApplicationContext(), "Please Fill up the username and password fields before.", Toast.LENGTH_SHORT);
+            toast_2.show();
+        }
+
     }
 }
